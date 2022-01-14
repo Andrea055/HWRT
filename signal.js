06d406ca98f7ee314cccfdf77e94831f590a6d5a
Andrea Canale <vivanco11.ac@gmail.com>
2022-01-15 00:54:12 +0100

    Update signal.js

--- signal.js
+++ signal.js
@@ -1,48 +1,38 @@
-var info2 = {
-    pci: '485',
-    sc: '',
-    cell_id: '36416268',
-    rsrq: '-12.0dB',
-    rsrp: '-80dBm',
-    rssi: '>=-51dBm',
-    sinr: '29dB',
-    rscp: '',
-    ecio: '',
-    mode: '7',
-    ulbandwidth: '20MHz',
-    dlbandwidth: '20MHz',
-    txpower: 'PPusch:-11dBm PPucch:-21dBm PSrs:0dBm PPrach:-18dBm',
-    tdd: '',
-    ul_mcs: 'mcsUpCarrier1:18',
-    dl_mcs: 'mcsDownCarrier1Code0:28 mcsDownCarrier1Code1:28',
-    earfcn: 'DL:3350 UL:21350',
-    rrc_status: '',
-    rac: '',
-    lac: '',
-    tac: '12558',
-    band: '7',
-    nei_cellid: 'No1:440',
-    plmn: '22288',
-    ims: '0',
-    wdlfreq: '',
-    lteulfreq: '25600',
-    ltedlfreq: '26800',
-    transmode: 'TM[4]',
-    enodeb_id: '0142251',
-    cqi0: '15',
-    cqi1: '15',
-    ulfrequency: '2560000kHz',
-    dlfrequency: '2680000kHz',
-    arfcn: '',
-    bsic: '',
-    rxlev: ''
-  }
 var scritti=[]
 const fs = require('fs'); 
+var info2={}
 var n=0
+const huaweiLteApi = require('huawei-lte-api');
+
+const connection = new huaweiLteApi.Connection('http://admin:pass@192.168.100.1/');
+
+connection.ready.then(function() {
+    console.log('Ready');
+
+
+    const device = new huaweiLteApi.Device(connection);
+    device.signal().then(function(result) {
+        info2=result
+    }).catch(function(error) {
+        console.log(error);
+    });
+
+});
 var i=0
 function update(){
     fs.appendFile('plotdata/' + n.toString() + ".json", JSON.stringify(info2), function (err) {
+      connection.ready.then(function() {
+        console.log('Ready');
+    
+    
+        const device = new huaweiLteApi.Device(connection);
+        device.signal().then(function(result) {
+            info2=result
+        }).catch(function(error) {
+            console.log(error);
+        });
+    
+    });
         if (err) throw err;
         console.log('Updated plot data!');
       }); 
