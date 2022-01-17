c74fe00a298371a5afd77b9030f0ccbf4130897a
Andrea Canale <vivanco11.ac@gmail.com>
2022-01-17 12:28:42 +0100

    prepare for other country

--- app/bts/btsitaly.js
+++ app/bts/btsitaly.js
@@ -1,9 +1,15 @@
 const fs = require('fs')
+    var path = window. location. pathname;
+    var page = path. split("/"). pop();
 
 fs.readFile('bts.json', 'utf8' , (err, data) => {
-    document.getElementById('bts').src="https://lteitaly.it/internal/map.php#bts=" + JSON.parse(data)[0] + "." + JSON.parse(data)[1]
+    if(page=="italy.html"){
+        document.getElementById('bts').src="https://lteitaly.it/internal/map.php#bts=" + JSON.parse(data)[0] + "." + JSON.parse(data)[1]
+    }
+
   if (err) {
     alert("Errore!")
     alert(err)
     return
 }})
+
